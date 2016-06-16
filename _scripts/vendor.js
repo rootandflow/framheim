e1154a9bbd16fa28d087d5eb32f25b2712294e2d
Remy Schrader <remy@rootandflow.com>
2016-06-15 23:29:14 -0700

    move init functions to site bundle

--- _scripts/vendor.js
+++ _scripts/vendor.js
@@ -1,14 +1,1 @@
-
-  WebFont.load({
-    google: {
-      families: ['Ovo', 'Muli']
-    }
-  });
-
-var foundation = require('foundation-sites');
-
-  $(document).ready(function($) {
-      $(document).foundation();
-  });
-
 console.log('vendor script loaded');
